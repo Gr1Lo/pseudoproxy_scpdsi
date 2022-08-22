0b0d802a08295d0715afd011a243604ef690df2c
Grigroriy Lozhkin <lozhkin.grig@gmail.com>
2022-08-22 18:53:46 +0300

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -115,3 +115,73 @@ def r_netCDF(f_path, min_lon = -145, min_lat = 14, max_lon = -52, max_lat = 71, 
       df_data = get_time_space(mean_df, time_dim = "year", lumped_space_dims = [coor[0],coor[1]])
 
     return df_data, ds_n, ret_lon, ret_lat
+
+
+def open_nc(df,
+            t_f_path, p_f_path, t_var_name, p_var_name, force0toNan=False, 
+            min_lon = -145, min_lat = 14, max_lon = -52, max_lat = 71):
+  
+    t_ds0 = xr.open_dataset(t_f_path)
+    p_ds0 = xr.open_dataset(p_f_path)
+    t_ds = t_ds0[t_var_name]
+    p_ds = p_ds0[p_var_name]
+    n_time = t_ds0["time"]
+
+    coor = [] 
+    for key in t_ds.coords.keys():
+      if key in ('lon','longitude'):
+        coor.append(key)
+    for key in t_ds.coords.keys():
+      if key in ('lat','latitude'):
+        coor.append(key)
+
+    #Выбор территории анализа
+    m_lon = np.where(t_ds.coords[coor[0]]>180, t_ds.coords[coor[0]]-360, t_ds.coords[coor[0]])
+    mask_lon = (m_lon >= min_lon) & (m_lon <= max_lon)
+    mask_lat = (t_ds.coords[coor[1]] >= min_lat) & (t_ds.coords[coor[1]] <= max_lat)
+    ret_lon = t_ds.coords[coor[0]][mask_lon].to_numpy()
+    ret_lat = t_ds.coords[coor[1]][mask_lat].to_numpy()
+
+    lat_lon_list = []
+    for fn in (df['file_name'].unique()):
+      df_t = df[df['file_name']==fn]
+      if ((df_t['lat'].values[0] >= min_lat) & (df_t['lat'].values[0] <= max_lat) 
+        & (df_t['lon'].values[0] >= min_lon) & (df_t['lon'].values[0] <= max_lon)):
+        lat_ind = (np.abs(df_t['lat'].values[0] - ret_lat)).argmin()
+        ret_lon0 = np.where(ret_lon>180, ret_lon-360, ret_lon)
+        lon_ind = (np.abs(df_t['lon'].values[0] - ret_lon0)).argmin()
+        if [ret_lat[lat_ind],ret_lon[lon_ind]] not in lat_lon_list:
+          lat_lon_list.append([ret_lat[lat_ind],ret_lon[lon_ind]])
+
+    t_ds_n = t_ds[:,mask_lat,mask_lon]
+    p_ds_n = p_ds[:,mask_lat,mask_lon]
+
+    t____ = t_ds_n.to_dataframe()
+    p____ = p_ds_n.to_dataframe()
+    l1 = t____.index.get_level_values(1)
+    l2 = t____.index.get_level_values(2)
+
+    all_cond = np.array(np.full(len(t____), False))
+    for i in lat_lon_list:
+      cond = np.array(((l1==i[0]) & (l2==i[1])))
+      all_cond = all_cond + cond
+
+    t_df_nn = t____[all_cond].reset_index()
+    p_df_nn = p____[all_cond].reset_index()
+
+
+    if force0toNan:
+      t_df_nn[t_df_nn==0] = np.nan
+      p_df_nn[p_df_nn==0] = np.nan
+
+    t_df_nn['month'] = np.repeat(n_time.dt.month, len(t_df_nn)/len(n_time.dt.month))
+    t_df_nn['year'] = np.repeat(n_time.dt.year, len(t_df_nn)/len(n_time.dt.year))
+    p_df_nn['month'] = np.repeat(n_time.dt.month, len(p_df_nn)/len(n_time.dt.month))
+    p_df_nn['year'] = np.repeat(n_time.dt.year, len(p_df_nn)/len(n_time.dt.year))
+
+    ret_lon = np.where(ret_lon>180, ret_lon-360, ret_lon)
+    #
+    t_df_nn['lon'] = np.where(t_df_nn['lon'] > 180, t_df_nn['lon']-360, t_df_nn['lon'])
+    p_df_nn['lon'] = np.where(p_df_nn['lon'] > 180, p_df_nn['lon']-360, p_df_nn['lon'])
+    #
+    return(t_df_nn, p_df_nn, t_var_name, p_var_name, ret_lon, ret_lat, [t_ds.units,p_ds.units], coor)
